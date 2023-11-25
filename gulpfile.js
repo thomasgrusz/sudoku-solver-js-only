bca79110be1bfea5f4d80fe35b623b7329ddc68a
Thomas Grusz <thomas.grusz@gmail.com>
2023-11-25 01:21:04 +0100

    chore: Add babel to gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser");
 const rename = require("gulp-rename");
 const del = require("del");
+const babel = require("gulp-babel");
 
 // Clean the "docs" folder
 function clean() {
@@ -17,15 +18,28 @@ function minifyHTML() {
     .pipe(gulp.dest("docs"));
 }
 
-// Minify JS files
-function minifyJS() {
-  return (
-    gulp
-      .src("src/js/*.js")
-      .pipe(terser())
-      // .pipe(rename({ suffix: '.min' }))
-      .pipe(gulp.dest("docs/js"))
-  );
+// // Minify JS files
+// function minifyJS() {
+//   return (
+//     gulp
+//       .src("src/js/*.js")
+//       .pipe(terser())
+//       // .pipe(rename({ suffix: '.min' }))
+//       .pipe(gulp.dest("docs/js"))
+//   );
+// }
+
+// Transpile and Minify JS files using Babel
+function transpileJS() {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(terser())
+    .pipe(gulp.dest("docs/js"));
 }
 
 // Copy contents of 'static' folder to 'docs'
@@ -38,18 +52,21 @@ function copyStatic() {
 // Watch for changes (optional)
 function watch() {
   gulp.watch("src/index.html", minifyHTML);
-  gulp.watch("src/js/*.js", minifyJS);
+  // gulp.watch("src/js/*.js", minifyJS);
+  gulp.watch("src/js/*.js", transpileJS);
   gulp.watch("static/**/*", copyStatic);
 }
 
 // Define a default task
 exports.default = gulp.series(
   clean,
-  gulp.parallel(minifyHTML, minifyJS, copyStatic),
+  // gulp.parallel(minifyHTML, minifyJS, copyStatic),
+  gulp.parallel(minifyHTML, transpileJS, copyStatic),
   watch
 );
 
 exports.clean = clean;
 exports.minifyHTML = minifyHTML;
-exports.minifyJS = minifyJS;
+// exports.minifyJS = minifyJS;
+exports.transpileJS = transpileJS;
 exports.copyStatic = copyStatic;
